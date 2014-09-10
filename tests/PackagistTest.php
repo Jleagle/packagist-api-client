98749be0fc58c51357257dd6429bc7367d7402ee
Jleagle <jimeagle@gmail.com>
2014-09-10 23:47:36 +0100

    Fix Travis

--- tests/PackagistTest.php
+++ tests/PackagistTest.php
@@ -4,32 +4,38 @@ use \Jleagle\Packagist;
 class PackagistTest extends PHPUnit_Framework_TestCase
 {
 
+  /**
+   * @group medium
+   *
+   * @throws Exception
+   */
   public function testPackage()
   {
-
     $packagist = new Packagist('http://packagist.org');
     $package = $packagist->package('jleagle/packagist-api-wrapper');
 
     $this->assertArrayHasKey('dev-master', $package);
     $this->assertArrayHasKey('name', $package['dev-master']);
-
   }
 
+  /**
+   * @group medium
+   */
   public function testSearch()
   {
-
     $packagist = new Packagist('http://packagist.org');
     $search = $packagist->search();
 
     $this->assertArrayHasKey('results', $search);
     $this->assertArrayHasKey('pages', $search);
     $this->assertEquals(ceil($search['total']/15), $search['pages']);
-
   }
 
+  /**
+   * @group medium
+   */
   public function testAll()
   {
-
     $packagist = new Packagist('http://packagist.org');
 
     $all = $packagist->all();
@@ -38,19 +44,19 @@ class PackagistTest extends PHPUnit_Framework_TestCase
     $filtered = $packagist->all('*zend*');
     $this->assertTrue(in_array('zendframework/zend-authentication', $filtered));
     $this->assertFalse(in_array('jleagle/packagist-api-wrapper', $filtered));
-
-
-
   }
 
+  /**
+   * @group medium
+   *
+   * @throws Exception
+   */
   public function testPackageException()
   {
     $this->setExpectedException('Exception');
 
     $packagist = new Packagist('http://packagist.org');
     $package = $packagist->package('xxx');
-
   }
-
 
 }
